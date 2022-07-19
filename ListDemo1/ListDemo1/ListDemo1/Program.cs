7b7c38acd686b28dd68700ac6f7b0da6c5436cfc
MD EMRUL EMRAN <emrulhasan@gmail.com>
2022-07-19 11:11:45 -0400

    loading sample list codes

---
+++ ListDemo1/ListDemo1/ListDemo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        List<string> names = new List<string>();
+            names.Add("Billal");
+            names.Add("Zaman");
+            names.Add("Sarowar");
+            names.Add("Kayes");
+            names.Add("Atik");
+            names.Add("Armel");
+            names.Add("Akter");
+            names.Add("Kayes");
+            names.Add("Bashiur");
+            names.Add("Mitu");
+
+            Console.WriteLine(names[1]);
+
+            Console.WriteLine(String.Join(", ", names));
+
+
+        }
+    }
+}
